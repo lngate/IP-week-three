72d2e010d268c6d76d89185813f614e84c93a2ba
Lewis Ng’ate <lewisngate@Lewiss-MacBook-Air.local>
2023-05-04 14:50:42 +0300

    amend on jquery code for hide and show

--- main scripts/main.js
+++ main scripts/main.js
@@ -1,9 +1,9 @@
-$(document).ready(function() {
-    $(".image-container").hover(function() {
+$(document).ready(()=> {
+    $(".image-container").hover(()=> {
       var text = $(this).data("text");
       $(this).find("img").hide();
       $(this).find(".hidden-text").text(text).show();
-    }, function() {
+    }, ()=> {
       $(this).find("img").show();
       $(this).find(".hidden-text").hide();
     });
